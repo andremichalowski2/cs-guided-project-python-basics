7bf0fd07dc95880a94943ad170f07289b142c82a
amichalo421 <andre.michalowski@gmail.com>
2020-11-30 18:23:09 -0500

    Demo 2

--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -1,13 +1,29 @@
 """
 Challenge #2:
-
+​
 Write a function that takes an integer `minutes` and converts it to seconds.
-
+​
 Examples:
 - convert(5) ➞ 300
 - convert(3) ➞ 180
 - convert(2) ➞ 120
+- convert(-10) -> -600 
 """
 def convert(minutes):
-    # Your code here
-
+    """
+    Input: int 
+    Output: int 
+​
+    What we received a negative integer?
+    1. Do the same thing  
+    2. Check if the input int >= 0, and only multiply by 60 if it is 
+​
+    We know that there are 60 seconds in a minute 
+    Given n minutes, we can convert that into seconds with the formula
+    n * 60 
+    """
+​
+    # return minutes * 60 
+​
+    answer = minutes * 60 
+    return answer
