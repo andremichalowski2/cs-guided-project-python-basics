3e14390a6032ebc79c426dc97068f25c79adc284
amichalo421 <andre.michalowski@gmail.com>
2020-11-30 20:24:49 -0500

    Demo 8

--- src/demonstration_08.py
+++ src/demonstration_08.py
@@ -1,8 +1,8 @@
 """
 Challenge #8:
-
+​
 Create a function that returns the number of arguments it was called with.
-
+​
 Examples:
 - num_args() ➞ 0
 - num_args("foo") ➞ 1
@@ -10,6 +10,8 @@ Examples:
 - num_args(True, False) ➞ 2
 - num_args({}) ➞ 1
 """
-def num_args():
+def num_args(*args):
     # Your code here
-
+    # Read more about *args and *kwargs here
+    # https://realpython.com/python-kwargs-and-args/
+    return len(args)
